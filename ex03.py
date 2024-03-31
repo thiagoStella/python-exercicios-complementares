7e4d464da3731fc2eee301dbd3eb487657fa83f1
thiagoStella <thiagostellapontes01@gmail.com>
2024-03-31 14:49:40 -0300

    ex3

--- ex03.py
+++ ex03.py
@@ -1,1 +1,5 @@
 '''Crie um programa que leia a largura e a altura de uma parede em metros e calcule a sua área e a quantidade de tinta a ser usada, cada litro de tinta pinta 2m²'''
+alt = float(input("Digite a altura da parede: "))
+larg = float(input("Digite a largura: "))
+area = alt * larg
+print("Para pintar a parede serão necessarios {:.2f} litros de tinta".format(area / 2))
