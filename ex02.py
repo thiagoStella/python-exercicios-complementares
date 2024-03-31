7574703b563ed76793a588df5e37e1af551ece33
thiagoStella <thiagostellapontes01@gmail.com>
2024-03-31 14:42:03 -0300

    ex2

--- ex02.py
+++ ex02.py
@@ -1,1 +1,3 @@
-'''Cri um algoritimo que leia um némero e mostre o seu dobro, seu triplo e a raiz quadrada.'''
+'''Crie um algoritimo que leia um número e mostre o seu dobro, seu triplo e a raiz quadrada.'''
+num = int(input("Digite um número: "))
+print("Dobro do número: {} \nTriplo: {} \nRaiz quadrada: {}".format(num * 2, num * 3, num ** 0.5))
