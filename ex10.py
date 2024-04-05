f19a43074c75dc4155338fec67e63f48fcb4daf8
thiagoStella <thiagostellapontes01@gmail.com>
2024-04-05 12:07:15 -0300

    ex10

---
+++ ex10.py
@@ -0,0 +1,17 @@
+'''. Escreva um algoritmo para verificar se uma palavra é um palíndromo usando o while'''
+palavra = input('Digite uma palavra: ')
+i = 0
+j = len(palavra) - 1
+palindromo = True
+
+while i < j:
+    if palavra[i] != palavra[j]:
+        palindromo = False
+        break
+    i += 1
+    j -= 1
+
+if palindromo:
+    print('A palavra é um palindromo')
+else:
+    print('A palavra não é um palindromo')
