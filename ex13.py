3eb501f911bb9f0ac84a383e1c9e41440781ad2e
thiagoStella <thiagostellapontes01@gmail.com>
2024-04-12 13:19:10 -0300

    ex13

---
+++ ex13.py
@@ -0,0 +1,15 @@
+for i in range(1,10):
+    print(f'Turma {i}')
+    print('==========')
+    for nome in range(1,25):
+        nome = input('Digite o nome do aluno: ')
+        media = 0
+        for n in range(1,6):
+            media += int(input(f'Digite a {n}ª nota: '))
+        media = media / 5
+
+for i in range(1,10):
+    print(f'Turma {i}')
+    print('==========')
+    for j in range(1,25):
+        print(f'O aluno {nome}, foi com a média {media}')
